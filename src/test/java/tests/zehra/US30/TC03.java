4603f07c72568a62cb480188475dbbb464aa0078
ZhrDemir <zdemir2702@gmail.com>
2024-01-10 23:07:00 +0300

    /10.01/zehra/US30/Configuration/admindashboard

--- src/test/java/tests/zehra/US30/TC03.java
+++ src/test/java/tests/zehra/US30/TC03.java
@@ -1,8 +1,12 @@
 package tests.zehra.US30;
 
+import com.github.javafaker.Faker;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 import pages.EasyBusTicketPage;
+import pages.admin.AdminDashBoardPage;
+import pages.admin.AdminLoginPage;
 import pages.user.UserLoginPage;
 import utilities.ConfigReader;
 import utilities.Driver;
@@ -11,18 +15,46 @@ import utilities.ReusableMethods;
 public class TC03 {
     @Test
     public void test01(){
-        Driver.getDriver().get(ConfigReader.getProperty("eBTUrl"));
-        EasyBusTicketPage easyBusTicketPage = new EasyBusTicketPage();
-        UserLoginPage userLoginPage = new UserLoginPage();
-        easyBusTicketPage.cookiesButton.click();
-        easyBusTicketPage.signInButton.click();
-        userLoginPage.usernameBox.sendKeys(ConfigReader.getProperty("userName"));
-        userLoginPage.passwordBox.sendKeys(ConfigReader.getProperty("invalidPass"));
-        userLoginPage.loginButton.click();
-        Assert.assertTrue(userLoginPage.slideInBox.isDisplayed());
+        // Browser'ı açar
+        // URL'e gider
 
-        ReusableMethods.wait(2);
-        Driver.closeDriver();
+        Driver.getDriver().get(ConfigReader.getProperty("eBTAdminUrl"));
+        AdminLoginPage adminLoginPage = new AdminLoginPage();
+        adminLoginPage.usernameBox.sendKeys(ConfigReader.getProperty("admin11"));
+        adminLoginPage.passwordBox.sendKeys(ConfigReader.getProperty("adminPass"));
+        adminLoginPage.loginButton.click();
+
+        // "Counter" linkine tıklar
+        AdminDashBoardPage adminDashBoardPage = new AdminDashBoardPage();
+        adminDashBoardPage.linkCounter.click();
+
+        // Açılan sayfada "Add New" butonunu görüntüler
+
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertTrue(adminDashBoardPage.addNewButton.isDisplayed(),"Add New butonu görünmüyor");
+
+        // "Add New" butonuna tıklar
+
+        adminDashBoardPage.addNewButton.click();
+
+        // İstenen bilgileri (Name, City, Location, Mobile) girer
+        Faker faker = new Faker();
+
+         adminDashBoardPage.inputName.sendKeys(faker.name().firstName());
+         adminDashBoardPage.inputCity.sendKeys(faker.address().city());
+         adminDashBoardPage.inputLocation.sendKeys(faker.address().cityName());
+         adminDashBoardPage.inputMobile.sendKeys(faker.phoneNumber().phoneNumber());
+
+        // Pencerenin altındaki "Save" butonunu görür ve tıklar
+        softAssert.assertTrue(adminDashBoardPage.saveButton.isDisplayed());
+        adminDashBoardPage.saveButton.click();
+
+        // Sayfanın en altına iner ve yeni Counter açtığını görür, Counter bigilerini görüntüler
+
+
+
+
 
     }
+
 }
