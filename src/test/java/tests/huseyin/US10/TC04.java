e130491003e48fb2417bb626b3400fb322f71a79
HariSeldonBE <bckbxl@gmail.com>
2024-01-14 21:47:45 +0100

    /14.01/HariSeldon/ waiting times are added for firefox browser

--- src/test/java/tests/huseyin/US10/TC04.java
+++ src/test/java/tests/huseyin/US10/TC04.java
@@ -34,6 +34,7 @@ public class TC04 {
         // Home Page Sign-in Button Enable Test
         softAssert.assertTrue(easyBusTicketPage.cookiesButton.isEnabled(),"Cookies Button is not enabled");
 
+        ReusableMethods.wait(2);
         easyBusTicketPage.cookiesButton.click();
 
         // Home Page Sign-in Button Display Test
@@ -41,6 +42,7 @@ public class TC04 {
         // Home Page Sign-in Button Enable Test
         softAssert.assertTrue(easyBusTicketPage.signInButton.isEnabled(),"Sign-In Button is not enabled");
 
+        ReusableMethods.wait(1);
         easyBusTicketPage.signInButton.click();
 
         String actualUrl = Driver.getDriver().getCurrentUrl();
@@ -68,13 +70,15 @@ public class TC04 {
         // Enter Invalid Username and Valid Password then Click to the Login button
         userLoginPage.usernameBox.sendKeys(ConfigReader.getProperty("invalidUserName"));
         userLoginPage.passwordBox.sendKeys(ConfigReader.getProperty("userPass"));
+        ReusableMethods.wait(1);
         userLoginPage.loginButton.click();
+        ReusableMethods.wait(1);
 
         // Invalid Login Notification Display Test at the Tap Right of teh Page
         softAssert.assertTrue(userLoginPage.slideInBox.isDisplayed());
 
+
         softAssert.assertAll();
-        ReusableMethods.wait(1);
         Driver.closeDriver();
     }
 }
