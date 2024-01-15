e4586b3bb8f1a4f6a5f0a45b09debe85c37204f0
Zeliha Selcuk <zeliha777@hotmail.com>
2024-01-15 16:06:03 +0300

    update

--- src/test/java/tests/zeliha/US28/TC01.java
+++ src/test/java/tests/zeliha/US28/TC01.java
@@ -31,7 +31,7 @@ public class TC01 {
    */
 
         
-        g
+
     }
 
 
--- src/test/java/tests/zeliha/US28/TC04.java
+++ src/test/java/tests/zeliha/US28/TC04.java
@@ -1,4 +1,43 @@
 package tests.zeliha.US28;
 
+import org.testng.annotations.Test;
+
 public class TC04 {
+
+    @Test
+    public void ClosedRequestTesti(){
+
+        /*
+        TC04 - Admin Dashboard menüsünde Support Request menüsü altında bulunan
+        "Closed Request" sayfasındaki talepler görüntülenebilmeli.
+
+        STEPLER
+
+        1 - Browser açılır.
+        2 -	URL'e gidilir.
+        3 -	Admin olarak giriş yapılır.
+        4 -	"Support Requests" linki tıklanır.
+        5 -	"Closed Request" linki tıklanır.
+        6 -	"Closed Request" başlığının görünür olduğu doğrulanır.
+        7 -	"Subject" başlık öğesinin ve altındaki ilgili bilet bilgisinin görünür olduğu doğrulanır.
+        8 -	"Submitted By"  başlık öğesinin ve altındaki ilgili bilet bilgisinin görünür olduğu doğrulanır.
+        9 -	"Status" başlık öğesinin ve altındaki ilgili bilet bilgisinin görünür olduğu doğrulanır.
+        10 - "Priority" başlık öğesinin ve altındaki ilgili bilet bilgisinin görünür olduğu doğrulanır.
+        11 - "Last Reply" başlık öğesinin ve altındaki ilgili bilet bilgisinin görünür olduğu doğrulanır.
+        12 - "Action" başlık öğesinin ve altındaki ilgili bilet bilgisinin görünür olduğu doğrulanır.
+        13 - "Action Details" ikonunun aktif olduğu doğrulanır.
+
+        */
+
+
+
+
+
+
+
+
+
+
+
+    }
 }
