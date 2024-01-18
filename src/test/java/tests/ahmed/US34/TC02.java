d34f9187adcf709db25abd6653d7c95bc76431bb
ZhrDemir <zdemir2702@gmail.com>
2024-01-18 12:52:26 +0300

    /18.01/zehra/ahmetin tc01/

--- src/test/java/tests/ahmed/US34/TC02.java
+++ src/test/java/tests/ahmed/US34/TC02.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 import pages.admin.AdminDashBoardPage;
+import pages.admin.AdminDashBoard_ProfilePage;
 import utilities.ConfigReader;
 import utilities.Driver;
 import utilities.ReusableMethods;
@@ -15,9 +16,28 @@ public class TC02 {
     public void test01(){
         SoftAssert softAssert = new SoftAssert();
         Actions actions = new Actions(Driver.getDriver());
-        AdminDashBoardPage adminDashBoardPage = new AdminDashBoardPage();
+        AdminDashBoard_ProfilePage adminDashBoard_profilePage = new AdminDashBoard_ProfilePage();
 
+        // Browser açılır
+        // Verilen URL ile ilgili web sitesine girilir
+        // Admin Username ve Password'u ilgili yerlere girilir
+        // Login butonuna tıklanır
         ReusableMethods.adminLoginMethod("admin13","123123123");
+
+        // Profile sayfasında bulunan "profile Information" box'unda
+        // Name, Email ve Image bilgilerinin değiştirilebildiği kontrol edilir
+        adminDashBoard_profilePage.linkProfile.click();
+        adminDashBoard_profilePage.ddmProfile.click();
+
+
+
+        // Gerekli değişiklikler yapıldıktan sonra Save Changes button'una basılır
+       // Değişiklikler kaydedildikten sonra profil bilgilerinin güncellendiğini kontrol edilir
+
+
+
+
+
 
         ReusableMethods.wait(2);
         Driver.quitDriver();
