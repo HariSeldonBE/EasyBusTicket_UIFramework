f97786479b9667dff38db178bcf0be590536b3b4
Ilteris Kaqan Colak <ilterisskaanncolakk@gmail.com>
2024-01-18 16:54:48 +0300

    18.01/ilteris/update/

--- src/test/java/tests/ilteris/US02/TC01.java
+++ src/test/java/tests/ilteris/US02/TC01.java
@@ -5,6 +5,7 @@ import org.testng.asserts.SoftAssert;
 import pages.EasyBusTicketPage;
 import utilities.ConfigReader;
 import utilities.Driver;
+import utilities.ReusableMethods;
 
 public class TC01 {
     @Test
@@ -26,11 +27,15 @@ public class TC01 {
 
         //Iletisim bilgilerinin görünürlügünü test edin
         softAssert.assertTrue(easyBusTicketPage.headerMobileNumberLinki.isDisplayed(),"Mobile Number görünüyor");
-        softAssert.assertTrue(easyBusTicketPage.headerEmail.isDisplayed(),"Email görünüyor");
+        softAssert.assertTrue(easyBusTicketPage.headerMobileNumberLinki.getAttribute("href").contains("tel:"), "Telefon linkine tıklayınca telefon uygulaması açılmıyor");
+
+
+        ReusableMethods.wait(1);
 
         //Iletisim bilgilerinin aktifliğini test edin
         easyBusTicketPage.headerMobileNumberLinki.click();
-        softAssert.assertTrue(easyBusTicketPage.headerMobileNumberLinki.isEnabled(),"Mobile Number'a erişilemiyor");
+        softAssert.assertTrue(easyBusTicketPage.headerEmail.getAttribute("href").contains("mailto:"), "Mail linkine tıklayınca mail uygulaması açılmıyor");
+
 
         easyBusTicketPage.headerEmail.click();
         softAssert.assertTrue(easyBusTicketPage.headerMobileNumberLinki.isEnabled(),"Email'e erişilemiyor");
